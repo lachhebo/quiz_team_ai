94333e7380dd21413f469c0e866a3555aef82cb0
Ismaël <ismail.lachheb@octo.com>
2020-05-13 11:22:06 +0200

    fix security issue

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -3,147 +3,60 @@ var quizQuestions = [
         question: "Ton sport préféré ?",
         answers: [
             {
-<<<<<<< HEAD
-                type: "Ismail",
+                type: "arthur",
                 content: "Soulever de la fonte"
             },
             {
-                type: "??",
+                type: "rofa",
                 content: "Le sport de chambre"
             },
             {
-                type: "??",
+                type: "lena, khna",
                 content: "Le marathon des bars"
             },
             {
-                type: "??",
+                type: "moal",
                 content: "Netflix and Chill"
             },
             {
-                type: "??",
+                type: "taha,isma",
                 content: "Je me sens libre quand je cours",
             },
             {
-                type: "Alessandro",
-=======
-                type: "oumi",
-                content: "Soulever de la fonte"
-            },
-            {
-                type: "oumi",
-                content: "Le sport de chambre"
-            },
-            {
-                type: "oumi",
-                content: "Le marathon des bars"
-            },
-            {
-                type: "oumi",
-                content: "Netflix and Chill"
-            },
-            {
-                type: "oumi",
-                content: "Je me sens libre quand je cours",
-            },
-            {
-                type: "oumi",
->>>>>>> result page rewrite wit react bootstrap
+                type: "theo",
                 content: "Le foot devant la télé et avec les copains",
             }
         ]
     },
     {
-<<<<<<< HEAD
-        question: "T'es plutôt Gif ou \"J\"if ?",
-        answers: [
-            {
-                type: "Ismail",
-                content: "G of course"
-            },
-            {
-                type: "Alessandro",
-                content: "Avec un \"J\" comme dans giraffe"
-            }
-        ]
-    },
-    {
-        question: "Comment se/s'est passé ton confinement ?",
-        answers: [
-            {
-                type: "Léa",
-                content: "Ni-quel ! J'ai juste oublié la crème solaire..."
-            },
-            {
-                type: "Alessandro",
-                content: "En tête à tête avec le mur de ma chambre de 4m2"
-            },
-            {
-                type: "??",
-                content: "Faites pas de gosses..."
-            },
-            {
-                type: "Ismail",
-                content: "J'ai appris à cuisiner"
-            }
-        ]
-    },
-    {
-        question: "Ta boisson préférée ? ",
-        answers: [
-            {
-                type: "??",
-                content: "La binouzze"
-            },
-            {
-                type: "Alessandro",
-                content: "Just a cup of tea"
-            },
-            {
-                type: "??",
-                content: "Coffee is my drug"
-            },
-            {
-                type: "??",
-                content: "Esprit sain dans un corps sain , je bois de l'eau"
-            },
-            {
-                type: "??",
-                content: "Un petit jaune"
-            }
-
-        ]
-    },
-
-=======
         question: "Tu prononce jif ou GIF ?",
         answers: [
             {
-                type: "oumi",
+                type: "theo",
                 content: "Soulever de la fonte"
             },
             {
-                type: "oumi",
+                type: "oumi, lena",
                 content: "Le sport de chambre"
             },
             {
-                type: "lena",
+                type: "taha",
                 content: "Le marathon des bars"
             },
             {
-                type: "oumi",
+                type: "khna",
                 content: "Netflix and Chill"
             },
             {
-                type: "oumi",
+                type: "arthur,isma",
                 content: "Je me sens libre quand je cours",
             },
             {
-                type: "oumi",
+                type: "moal",
                 content: "Le foot devant la télé et avec les copains",
             }
         ]
     }
->>>>>>> result page rewrite wit react bootstrap
 ];
 
 export default quizQuestions;
